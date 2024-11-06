f1f98a35f162fb80e55775abe6376292443c2f0d
Helios-rabiller-LP <helios.rabiller@laplateforme.io>
2024-11-06 13:05:21 +0100

    changement_helios

---
+++ morpion_fonction_victoire.py
@@ -0,0 +1,11 @@
+def victoire(Joueur):
+    condition_win = [[0,1,2],[3,4,5],[6,7,8],[0,4,8],[2,4,6],[0,3,6],[1,4,7],[2,5,8]]
+    if Joueur := condition_win == Joueur :
+      #  return True
+        print(" joueur X gagné")
+    else:
+     #   return False
+        print("retry")
+    if Joueur in [0,1,2] == True or :
+
+victoire([0,1,2])
