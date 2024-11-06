b096978c881056f21319a001d275beb2903da32e
cesar-bousson <cesar.bousson@laplateforme.io>
2024-11-06 13:37:13 +0100

    modification_cesar

---
+++ Grille_tictactoe.py
@@ -0,0 +1,11 @@
+
+def the_grids(grille):
+    print(f"{grille[0]}   | {grille[1]}   | {grille[2]}")
+    print("----+----+----")
+    print(f"{grille[3]}   | {grille[4]}   | {grille[5]}")
+    print("----+----+----")
+    print(f"{grille[6]}   | {grille[7]}   | {grille[8]}")
+        
+grille= ['1', '2', '3', '4', '5', '6', '7', '8','9']
+
+the_grids(grille)
