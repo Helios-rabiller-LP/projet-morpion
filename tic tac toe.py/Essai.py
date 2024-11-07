b66073309b1ad4e3620d485a3310710a40ad6aec
Anna marras <anna-marras@laplateforme.io>
2024-11-07 14:59:38 +0100

    Mise a jour

--- tic tac toe.py/Essai.py
+++ tic tac toe.py/Essai.py
@@ -1,10 +1,96 @@
 
-structure = [ " " for _ in range(9)]
+def start():
+    gamer_one()
+    structure()
 
-def afficher_board(structure):
-    for i in range(3):
-        print(structure[3*i] + " | " + structure[3*i + 1] + " | " + structure[3*i + 2])
-        if i <2:
-            print("--+--+--")
+grille =["-","-","-"
+        ,"-","-","-"
+        ,"-","-","-"]
 
-afficher_board(structure)
+# Le joueur choisit son symbole
+joueur_actuel = ""
+end_game = False
+gagnant = ""
+
+def jouer():
+    gamer_one()
+    affichage_grille()
+    while end_game == False :
+        tour(gamer_one)
+        verifier_fin_de_jeu(i)
+
+def gamer_one():
+    global gamer_one
+    gamer_one = input("Please, choose (X) or (O) : ")
+    while True:
+        gamer_one = gamer_one.upper()
+        if gamer_one == "X" :
+            print("You choose X. Gamer two have O")
+            break
+        elif gamer_one == "O" :
+            print("You choose O. Gamer two have X")
+            break
+        else:
+            gamer_one = input("Please, choose (X) or (O) : ")
+
+# Définir la grille de jeu
+def structure():
+    print("\n")
+    print("---------------")
+    print( " |" ,grille[0], "|" ,grille[1], "|" ,grille[2], "| " + "    1 | 2 | 3" )
+    print("---------------")
+    print( " |" ,grille[3], "|" ,grille[4], "|" ,grille[5], "| " + "    4 | 5 | 6" )
+    print("---------------")
+    print( " |" ,grille[6], "|" ,grille[7], "|" ,grille[8], "| " + "    7 | 8 | 9" )
+    print("---------------")
+    print("\n")
+
+
+def turn(gamer) :
+    print("It's turn of gamer : ", gamer)
+    pos = input("Veuillez sélectionner un espace vide sur la grille entre 1 et 9 : ")
+
+    valide = False
+    while valide == False :
+
+        while pos not in ["1","2","3","4","5","6","7","8","9"] :
+            pos = input("Veuillez sélectionner un espace vide sur la grille entre 1 et 9 : ")
+        pos = int(pos) - 1
+
+        if grille[pos] == "-" :
+            valide = True
+        else :
+            print("Vous ne pouvez pas accéder à cette postition")
+
+    grille[pos] = gamer_one
+    affichage_grille()
+
+def verififer_victoire():
+    global end_game
+    global gagnant
+    if grille[0] == grille[1] == grille[2] and grille[2] != "-" :
+        end_game = True
+        gagnant = grille[1]
+    if grille[3] == grille[4] == grille[5] and grille[5] != "-" :
+        end_game = True
+        gagnant = grille[2]
+    if grille[6] == grille[7] == grille[8] and grille[8] != "-" :
+        end_game = True
+        gagnant = grille[3]
+    if grille[0] == grille[1] == grille[2] and grille[2] != "-" :
+        end_game = True
+        gagnant = grille[1]
+
+
+
+start()
+
+
+
+
+
+
+
+
+
+
---
+++ tic tac toe.py/Essai2.py
@@ -0,0 +1,65 @@
+def afficher_plateau(plateau):
+    """Affiche le plateau de jeu."""
+    for ligne in plateau:
+        print("|".join(ligne))
+        print("-----")
+
+
+def verifier_victoire(plateau, symbole):
+    """Vérifie s'il y a une victoire pour le symbole donné."""
+    # Vérification des lignes
+    for ligne in plateau:
+        if all(case == symbole for case in ligne):
+            return True
+
+    # Vérification des colonnes
+    for colonne in range(3):
+        if all(plateau[ligne][colonne] == symbole for ligne in range(3)):
+            return True
+
+    # Vérification des diagonales
+    if plateau[0][0] == plateau[1][1] == plateau[2][2] == symbole:
+        return True
+    if plateau[0][2] == plateau[1][1] == plateau[2][0] == symbole:
+        return True
+
+    return False
+
+
+def jeu_tic_tac_toe():
+    """Fonction principale pour jouer au Tic-Tac-Toe."""
+    plateau = [[" " for _ in range(3)] for _ in range(3)]
+    symboles = ["X", "O"]
+    joueur = 0  # Joueur 1 commence
+
+    while True:
+        afficher_plateau(plateau)
+        print(f"C'est au joueur {joueur + 1} ({symboles[joueur]}) de jouer.")
+
+        # Demander au joueur de choisir une position
+        while True:
+            try:
+                ligne = int(input("Choisissez la ligne (1-3) : ")) - 1
+                colonne = int(input("Choisissez la colonne (1-3) : ")) - 1
+                if plateau[ligne][colonne] == " ":
+                    plateau[ligne][colonne] = symboles[joueur]
+                    break
+                else:
+                    print("Cette case est déjà occupée. Choisissez une autre position.")
+            except (ValueError, IndexError):
+                print("Entrée invalide. Veuillez saisir un nombre entre 1 et 3.")
+
+        # Vérifier s'il y a une victoire
+        if verifier_victoire(plateau, symboles[joueur]):
+            afficher_plateau(plateau)
+            print(f"Le joueur {joueur + 1} ({symboles[joueur]}) remporte la partie !")
+            break
+
+        # Vérifier s'il y a une égalité
+        if all(plateau[ligne][colonne] != " " for ligne in range(3) for colonne in range(3)):
+            afficher_plateau(plateau)
+            print("Match nul !")
+            break
+
+        # Passer au joueur suivant
+        joueur = (joueur + 1) % 2
